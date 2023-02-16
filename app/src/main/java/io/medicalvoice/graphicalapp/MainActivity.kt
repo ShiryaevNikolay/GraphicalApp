8e9ef32ed967fabca9841ecedce393a6334e1c35
ShiryaevNikolay <shiryaev@surfstudio.ru>
2023-02-16 19:02:51 +0300

    Отображение треугольника

--- app/src/main/java/io/medicalvoice/graphicalapp/MainActivity.kt
+++ app/src/main/java/io/medicalvoice/graphicalapp/MainActivity.kt
@@ -1,11 +1,50 @@
 package io.medicalvoice.graphicalapp
 
+import android.app.ActivityManager
+import android.content.Context
+import android.content.pm.ConfigurationInfo
+import android.opengl.GLSurfaceView
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+
+    private var mGLSurfaceView: GLSurfaceView? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        mGLSurfaceView = GLSurfaceView(this)
+
+        // Check if the system supports OpenGL ES 2.0.
+        val activityManager = getSystemService(Context.ACTIVITY_SERVICE) as ActivityManager
+        val configurationInfo: ConfigurationInfo = activityManager.deviceConfigurationInfo
+        val supportsEs2: Boolean = configurationInfo.reqGlEsVersion >= 0x20000
+
+        if (supportsEs2) {
+            // Request an OpenGL ES 2.0 compatible context.
+            mGLSurfaceView?.setEGLContextClientVersion(2)
+
+            // Set the renderer to our demo renderer, defined below.
+            mGLSurfaceView?.setRenderer(FirstOpenGLProjectRenderer())
+        } else {
+            // This is where you could create an OpenGL ES 1.x compatible
+            // renderer if you wanted to support both ES 1 and ES 2.
+            return
+        }
+
+        setContentView(mGLSurfaceView)
+    }
+
+    override fun onResume() {
+        // The activity must call the GL surface view's onResume() on activity onResume().
+        super.onResume()
+        mGLSurfaceView?.onResume()
+    }
+
+    override fun onPause() {
+        // The activity must call the GL surface view's onPause() on activity onPause().
+        super.onPause()
+        mGLSurfaceView?.onPause()
     }
 }
