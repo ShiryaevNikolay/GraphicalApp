c8b87822040f23ab33960c3e4236fc41904e5cbe
ShiryaevNikolay <shiryaev@surfstudio.ru>
2023-03-09 12:27:58 +0300

    Текстуры и освещение

--- app/src/main/java/io/medicalvoice/graphicalapp/MainActivity.kt
+++ app/src/main/java/io/medicalvoice/graphicalapp/MainActivity.kt
@@ -1,50 +1,39 @@
 package io.medicalvoice.graphicalapp
 
+import android.app.Activity
 import android.app.ActivityManager
-import android.content.Context
-import android.content.pm.ConfigurationInfo
 import android.opengl.GLSurfaceView
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 
-class MainActivity : AppCompatActivity() {
-
-    private var mGLSurfaceView: GLSurfaceView? = null
-
+class MainActivity : Activity() {
+    private var glSurfaceView: GLSurfaceView? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        mGLSurfaceView = GLSurfaceView(this)
-
-        // Check if the system supports OpenGL ES 2.0.
-        val activityManager = getSystemService(Context.ACTIVITY_SERVICE) as ActivityManager
-        val configurationInfo: ConfigurationInfo = activityManager.deviceConfigurationInfo
-        val supportsEs2: Boolean = configurationInfo.reqGlEsVersion >= 0x20000
-
-        if (supportsEs2) {
-            // Request an OpenGL ES 2.0 compatible context.
-            mGLSurfaceView?.setEGLContextClientVersion(2)
-
-            // Set the renderer to our demo renderer, defined below.
-            mGLSurfaceView?.setRenderer(FirstOpenGLProjectRenderer())
-        } else {
-            // This is where you could create an OpenGL ES 1.x compatible
-            // renderer if you wanted to support both ES 1 and ES 2.
+        if (!supportES2()) {
+            Toast.makeText(this, "OpenGL ES 2.0 is not supported", Toast.LENGTH_LONG).show()
+            finish()
             return
         }
-
-        setContentView(mGLSurfaceView)
-    }
-
-    override fun onResume() {
-        // The activity must call the GL surface view's onResume() on activity onResume().
-        super.onResume()
-        mGLSurfaceView?.onResume()
+        glSurfaceView = GLSurfaceView(this)
+        glSurfaceView!!.setEGLContextClientVersion(2)
+        glSurfaceView!!.setRenderer(OpenGLRenderer(this))
+        setContentView(glSurfaceView)
     }
 
     override fun onPause() {
-        // The activity must call the GL surface view's onPause() on activity onPause().
         super.onPause()
-        mGLSurfaceView?.onPause()
+        glSurfaceView!!.onPause()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        glSurfaceView!!.onResume()
+    }
+
+    private fun supportES2(): Boolean {
+        val activityManager = getSystemService(ACTIVITY_SERVICE) as ActivityManager
+        val configurationInfo = activityManager.deviceConfigurationInfo
+        return configurationInfo.reqGlEsVersion >= 0x20000
     }
 }
